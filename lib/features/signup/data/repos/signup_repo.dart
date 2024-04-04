4562429576b723d91ccd3d758dd7f5ccc8935dc2
Riad Haidar <71490833+RiadHaidar@users.noreply.github.com>
2024-04-04 03:15:27 -0400

    added signup feature logic layer

--- lib/features/signup/data/repos/signup_repo.dart
+++ lib/features/signup/data/repos/signup_repo.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutteradvnced/core/networking/api_result.dart';
 import 'package:flutteradvnced/core/networking/api_service.dart';
 import 'package:flutteradvnced/features/signup/data/models/signup_request_body.dart';
@@ -7,28 +5,19 @@ import 'package:flutteradvnced/features/signup/data/models/signup_response.dart'
 
 import '../../../../core/networking/api_error_handler.dart';
 
-class SignUpRepo{
-final ApiService _apiService;
+class SignUpRepo {
+  final ApiService _apiService;
 
-SignUpRepo(this._apiService);
+  SignUpRepo(this._apiService);
 
-Future <ApiResult<SingUpResponse>> signup(SignUpRequestBody signUpRequestBody )async{
-
-      try{
-
-        final response =await _apiService.signup(signUpRequestBody);
+  Future<ApiResult<SingUpResponse>> signup(
+      SignUpRequestBody signUpRequestBody) async {
+    try {
+      final response = await _apiService.signup(signUpRequestBody);
 
       return ApiResult.success(response);
-
-
-
-      }catch(e){
-              return ApiResult.failure(ErrorHandler.handle(e));
-
-
-      }
-
-
-}
-
+    } catch (e) {
+      return ApiResult.failure(ErrorHandler.handle(e));
+    }
+  }
 }
